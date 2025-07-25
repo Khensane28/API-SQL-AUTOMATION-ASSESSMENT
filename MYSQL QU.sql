7f96d969bb783d8f3e16b91b02c650e219e30e61
Khensane28 <150665459+Khensane28@users.noreply.github.com>
2025-07-25 16:20:30 +0200

    Update MYSQL QU.sql

--- MYSQL QU.sql
+++ MYSQL QU.sql
@@ -1,18 +1,32 @@
-SELECT 
-    CONCAT(c.first_name, ' ', c.last_name) AS "Full Name", 
-    c.email AS "Email Address", 
-    n.note_text AS "Most Recent Note" 
-FROM contacts c  
-LEFT JOIN contact_notes n 
-    ON c.contact_id = n.contact_id 
-    AND n.note_id = (
-        SELECT n2.note_id
-        FROM contact_notes n2 
-        WHERE n2.contact_id = c.contact_id 
-        ORDER BY n2.created_at DESC
-        LIMIT 1
-    )
-WHERE 
-    c.first_name LIKE 'A%'
-    AND (n.created_at >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) OR n.created_at IS NULL)
-ORDER BY c.last_name, c.first_name;
+SELECT * FROM contacts
+WHERE first_name LIKE 'A%'
+
+    SELECT contacts.id,contact_notes.contact_id
+FROM contacts
+LEFT JOIN contact_notes ON contacts.id = contact_notes.contact_id
+ORDER BY contacts.id
+WHERE created_at >=CURRENT_DATE -INTERVAL '7 days';
+
+SELECT contacts.first_name +' ' + contacts.last_name as full_name,
+contacts.email,
+SELECT contacts.id,contact_notes.contact_id
+FROM contact
+FULL OUTER JOIN contact_notes ON contacts.id = contact_notes.contact_id
+ORDER BY contacts.id
+WHERE created_at >=CURRENT_DATE -INTERVAL '7 days';
+FINAL:
+SELECT
+contacts.first_name ||' '|| contacts.last_name AS full_name,
+contacts.emails,
+contact_notes.note_text AS most_recent_note
+FROM contacts
+JOIN(SELECT 
+contact_id,
+note_text,
+ROW_NUMBER() OVER (PARTITION BY contact_id ORDER BY created_at DESC) AS rn
+FROM contact_notes
+WHERE created at >= CURRENT_DATE - INTERVAL '7 days')AS n
+ON contact_notes.contact_id=contacts.id
+AND contact_notes.rn =1 WHERE contacts.first_name LIKE 'A%'
+ORDER BY full_name;
+
